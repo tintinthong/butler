4d50d0b7f19b6c51ff0f7937a9b383a32cb688ad
Justin <justinthong93@gmail.com>
2020-08-17 23:11:29 +0800

    Adding persistent models

--- src/Database/Migrate.hs
+++ src/Database/Migrate.hs
@@ -12,33 +12,16 @@
 {-# LANGUAGE UndecidableInstances       #-}
 import           Control.Monad.IO.Class  (liftIO)
 import           Database.Persist
+import           Database.Persist.Quasi
 import           Database.Persist.Sqlite
 import           Database.Persist.TH
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Person
-    name String
-    age Int Maybe
-    deriving Show
-BlogPost
-    title String
-    authorId PersonId
-    deriving Show
-|]
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] $(persistFileWith lowerCaseSettings "./Models.persistentmodels")
+
 
 doMigrate :: IO ()
 doMigrate = runSqlite "../../butler.db" $ do
     runMigration migrateAll
-    johnId <- insert $ Person "Justin Thong" $ Just 35
-    janeId <- insert $ Person "Laura DreyFuzz" Nothing
-    insert $ BlogPost "My fr1st p0st" johnId
-    insert $ BlogPost "One more for good measure" johnId
-
-    oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
-    liftIO $ print (oneJohnPost :: [Entity BlogPost])
-
-    john <- get johnId
-    liftIO $ print (john :: Maybe Person)
-
-    delete janeId
-    deleteWhere [BlogPostAuthorId ==. johnId]
+    justinId <- insert $ User "Justin Thong" "justinthong93@gmail.com" True
+    justin <- get justinId
+    liftIO $ print (justin :: Maybe User)
